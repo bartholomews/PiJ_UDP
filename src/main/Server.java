b3c41f91596f7d56f77b895a985957143d8e95cb
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-04-13 06:33:12 +0100

    updated interface Server - sendString(SocketStream, String)

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.net.Socket;
 
 /**
  * The Server class takes connection requests from multiple Clients over TCP
@@ -15,21 +16,35 @@ import java.io.IOException;
  */
 public interface Server {
     /**
-     * Initialise the main ServerSocket on a specified port and start listening for connections.
+     * Initialise the main ServerSocket on a fixed port and start listening for connections.
      * For each accepted connection a communication stream should be opened via openStream(Socket) method.
      * The Server should concentrate on listening new connections and leave their handling to other threads.
      *
-     * @throws java.io.IOException
+     * @throws IOException for a communication error.
      */
     public void init() throws IOException;
 
+    /**
+     * Initialise the main ServerSocket on a port selected as parameter, and start listening for connections.
+     * For each accepted connection a communication stream should be opened via openStream(Socket) method.
+     * The Server should concentrate on listening new connections and leave their handling to other threads.
+     *
+     * @param port the port to connect to.
+     * @throws IOException for a communication error.
+     */
     public void init(int port) throws IOException;
 
     /**
-     * Open a communication stream with a Client socket (wrapped in a {@see SocketStream})
-     * and send a String message through it.
+     * Writes out a String message to a Client socket (wrapped inside a {@see SocketStream}).
+     *
+     * @param sock the SocketStream wrapping an InputStream/OutputStream that reads/writes to the Client socket.
+     * @param message the String message to be sent via the SocketStream.
+     * @return true after the Stream is flushed.
+     * @throws IOException for a communication error.
      */
     public boolean sendString(SocketStream sock, String message) throws IOException;
+
+    public boolean handleRequest(Socket socket);
 
     // TODO thread pool UDP class and here a method to send the connection details to that class (and to the Client)
 
