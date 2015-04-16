2733389831353f8766af92c2cae53619269d105a
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-04-16 18:46:05 +0100

    updated interface Connection - isSender(boolean) and  Javadoc

--- src/main/Connection.java
+++ src/main/Connection.java
@@ -5,16 +5,15 @@ import java.net.Socket;
 /**
  * A Connection is a Client-end socket associated with an unique ID given by the Server which is connected to,
  * and a boolean value which tells whether it is the first Client to be connected or not.
- * Every Client which establishes a connection with the Server should get an instance of this class.
  *
  * @author federico.bartolomei (BBK-PiJ-2014-21)
  */
 public interface Connection {
 
     /**
-     * The connection socket between the Client and the Server.
+     * The Client socket opened with the Server
      *
-     * @return the connection socket of the Client with the Server.
+     * @return the Client socket opened with the Server.
      */
     public Socket getSocket();
 
@@ -23,13 +22,20 @@ public interface Connection {
      *
      * @return the unique ID of the Client connected with the Server.
      */
-    public String getID();  // todo implement UUID?
+    public String getID();  // todo implement UUID? from the Server class?
 
     /**
-     * Boolean flag which checks whether the Connection is the "oldest" in communication with the Server.
+     * Boolean flag to check whether the Connection is the "oldest" in communication with the Server.
      *
-     * @return true if it is the first (i.e. "oldest") active Client connected with the Server, false otherwise.
+     * @return true if it is the first (i.e. "sender") active Client connected with the Server, false otherwise.
      */
     public boolean isSender();
 
+    /**
+     * Change the state of isSender the boolean flag. This might happen when the first Connection disconnects,
+     * and the second one is promoted to sender_status.
+     *
+     * @param isSender the new flag status of the Client connection, true if "sender", false if "receiver".
+     */
+    public void setStatus(boolean isSender);
 }
