90c27a18eb78e766c1ae7f6bf6a73e1ed831c699
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-04-20 02:48:14 +0100

    updated Javadoc

--- src/main/ThreadsPool.java
+++ src/main/ThreadsPool.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * Pool of threads to manage the Client requests. Each request is sent by the main server
- * via the method submit(socket). The pool will translate the socket with a {@see Connection} instance,
+ * via the method handleRequest(socket). The pool will translate the socket to a {@see Connection} instance,
  * which wraps the socket with useful information about that connection (ID, sender/receiver status).
  * It should keep a list of Connections to keep under control the first of the list (the sender).
  * The point-to-point TCP communication with each Client is dealt with working threads executed from the pool.
@@ -14,9 +14,10 @@ import java.util.List;
  */
 public interface ThreadsPool {
 
-    /** TODO
+    /**
      * Receive a socket and wraps it in a {@see Connection}. Add the connection on a List, and start a working
      * thread to keep the TCP communication with the Client connected with that socket.
+     * This should be done on a separated Runnable thread.
      *
      * @param socket the socket connected with a Client.
      */
@@ -25,7 +26,7 @@ public interface ThreadsPool {
     /**
      * The list of active Connections. It might be empty.
      * The first Connection in the list identify a socket, ID and client_status of "sender",
-     * all the others of "receiver". This is related on the UDP audio-transmission between client TODO
+     * all the others of "receiver".
      *
      * @return a List, maybe empty, of active Connections.
      */
