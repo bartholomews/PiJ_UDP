52cfe35db17f8a5c9f5aea48b3cfeefda0229096
BBK-PiJ-2014-21 <federico.bartolomei3@studio.unibo.it>
2015-04-20 01:26:13 +0100

    updated ClientImpl - connect() to return void

--- src/main/ClientImpl.java
+++ src/main/ClientImpl.java
@@ -1,35 +1,42 @@
 package main;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
 import java.util.UUID;
 
 /**
+ *  // TODO try with resources bufferedreader and printwriter as fields bound at construction time?
  *
  * @author federico.bartolomei (BBK-PiJ-2014-21)
  */
 public class ClientImpl implements Client {
+    // private MulticastSocket multicastSocket TODO
 
     /**
      * {@inheritDoc}
      *
      * @param hostname the hostname (or IP address) of the Server to connect to.
-     * @param port the port number to connect to.
+     * @param port     the port number to connect to.
      * @return the Client socket opened with the Server.
      * @throws java.io.IOException for a communication error with the Server.
      */
     @Override
-    public Socket connect(String hostname, int port) throws IOException {
-        try  {
+    public void connect(String hostname, int port) throws IOException {
+        try {
             Socket socket = new Socket(hostname, port);
-            getString(socket);
+            System.out.println("From " + socket.getRemoteSocketAddress() + ": " + getString(socket));
             getID(socket);
-            System.out.println("Status received: " + getStatus(socket));
-            System.out.println("Status received: " + getStatus(socket));
-            // manageUDP TODO
-            return socket;
+            ClientStatus status = getStatus(socket);
+            System.out.println("Status received: " + status.name());
+   /*   TODO SEND AUDIO VIA UDP
+            if (status == ClientStatus.SENDER) {
+                sendAudioChunks();
+            } else {
+               getAudioChunks();
+            }
+   */
         } catch (IOException ex) {
-            throw new IOException();
+            System.out.println("Error during connection with the Server");
         }
     }
 
@@ -41,30 +48,29 @@ public class ClientImpl implements Client {
      * @throws IOException for an error during connection or if a null String is received
      */
     public String getString(Socket socket) throws IOException {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String received = in.readLine();
-            if (received == null) {
-                throw new IOException("Connection opened, but Server failed to respond");
-            } else {
-                System.out.println("<From " + socket.getRemoteSocketAddress() + "> \"" + received + "\"");
-                return received;
-            }
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String received = in.readLine();
+        if (received == null) {
+            throw new IOException("Connection opened, but Server failed to respond");
+        } else {
+            return received;
+        }
     }
 
     /**
      * {@inheritDoc}
      *
-     * @param socket the socket connected with the Server
+     * @param socket  the socket connected with the Server
      * @param request the Client request to be sent to the Server
      * @throws IOException for an error during connection
      * @throws NullPointerException for a null Request
      */
     @Override
     public void sendRequest(Socket socket, Request request) throws IOException {
-        if(request == null) {
+        if (request == null) {
             throw new NullPointerException("Cannot have a null request");
         }
-        try  {
+        try {
             PrintWriter out = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
             String toSend = request.name();
             out.println(toSend);
@@ -100,15 +106,11 @@ public class ClientImpl implements Client {
         sendRequest(socket, Request.ID);
         String id = getString(socket);
         UUID.fromString(id);
-        System.out.println("Received id " + id + " from " + socket.getRemoteSocketAddress());
+        System.out.println("ID received: " + id + " from " + socket.getRemoteSocketAddress());
         return id;
     }
 
-    /* TODO
-    public void manageUDP(Socket socket) throws IOException {
-        (getStatus(socket) == ClientStatus.SENDER) ? sendAudio : receiveAudio;
-    }
-    */
-
-
 }
+
+
+
